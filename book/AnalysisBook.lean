178e14139240db7c177889a81febcd1355b9e9d5
Andrii Kurdiumov <kant2002@gmail.com>
2025-06-29 18:08:43 +0500

    Other experiment

--- book/AnalysisBook.lean
+++ book/AnalysisBook.lean
@@ -23,8 +23,7 @@ def theme : Theme := { Theme.default with
           <link rel="stylesheet" href="/static/style.css"/>
           <script>"window.__versoSiteRoot=\"/analysis/\""</script>
           {{← builtinHeader }}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q9C7QSY9LY"></script>
-
+          <script>{{googleAnalytics}}</script>
         </head>
         <body>
           <header>
